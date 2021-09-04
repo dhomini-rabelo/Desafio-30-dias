48c69fbb92134a824431ed7a3bd87973322c77e9
Dhomini Fael <dhominisalgado@gmail.com>
2021-09-04 14:37:38 -0300

    importar react

--- DAY#02/my-site/src/App.js
+++ DAY#02/my-site/src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
+import imgPeoples from './images/peoples.svg';
 
 function App() {
   return (
-    <>
-    oi
-    </>
+    <div class="reactApp">
+    <img src={imgPeoples} alt="imagem-pessoas" />
+    <textarea name="" id="" cols="30" rows="10"></textarea>
+    <button>COMENTAR</button>
+    </div>
   );
 }
 
