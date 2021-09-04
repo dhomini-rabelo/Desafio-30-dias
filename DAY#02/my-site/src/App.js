b7c8dc1a245c418c35b00d659e565c6027b0ec68
Dhomini Fael <dhominisalgado@gmail.com>
2021-09-04 15:29:16 -0300

    FUNCIONALIDADES

--- DAY#02/my-site/src/App.js
+++ DAY#02/my-site/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import imgPeoples from './images/peoples.svg';
 
 function App() {
+
+  const [commentary, setCommentary] = useState()
+  const [allCommentaries, setAllCommentaries] = useState([])
+
+  function comment(){
+    const currentAllCommentaries = [... allCommentaries, commentary] 
+    setAllCommentaries(currentAllCommentaries)
+  }
+
+  function weText(event){
+    setCommentary(event.target.value)
+  }
+  
+
+
   return (
-    <div class="reactApp">
-    <img src={imgPeoples} alt="imagem-pessoas" />
-    <textarea name="" id="" cols="30" rows="10"></textarea>
-    <button>COMENTAR</button>
+    <div>
+    <img src={imgPeoples} alt="peoples-image" />
+    <textarea onChange={weText} cols="40" rows="4"></textarea>
+    <button onClick={comment}>COMENTAR</button>
+    <ul>
+    {allCommentaries.map((commentary) => (
+      <li key={allCommentaries.indexOf(commentary)}>{commentary}</li>
+    ))}
+    </ul>
     </div>
   );
 }
