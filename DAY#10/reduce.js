4ffef7d270a6bd42088740f23ef4d67ccf0a9e47
Dhomini Fael <dhominisalgado@gmail.com>
2021-09-11 23:56:39 -0300

    pequena mudança no reduce

--- DAY#10/reduce.js
+++ DAY#10/reduce.js
@@ -1,5 +1,6 @@
 const companySalaries = [1000, 1500, 2000, 3000, 4000, 5000]
+const initialValueInAccumulator = 0
 
-const totalExpenses = companySalaries.reduce((accumulator, item) => accumulator + item)
+const totalExpenses = companySalaries.reduce((accumulator, item) => accumulator + item, initialValueInAccumulator)
 
 console.log(totalExpenses)
