1d6db760d7969a0caa28a05495f0830f7109e7bb
Dhomini Fael <dhominisalgado@gmail.com>
2021-09-11 23:40:50 -0300

    função map

---
+++ DAY#10/map.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNumbers = numbers.map((number) => number * number)
+
+// map functions can receive index and array -> (item, index, array)
+
+
+console.log(newNumbers)
