46f705597ae349a77beb20fb78ee7b8597204947
Dhomini Fael <dhominisalgado@gmail.com>
2021-09-13 17:11:54 -0300

    finalizando botões extras e projeto

--- CALCULATOR/script.js
+++ CALCULATOR/script.js
@@ -6,7 +6,7 @@ weButtons.forEach((button) => button.addEventListener('click', appendDigit))
 
 
 function appendDigit(event){
-    endings = {'=': 0, 'AC': 0}
+    endings = {'=': 0, 'AC': 0, '+/-': 0}
 
     if (!(event.target.innerText in endings)){
         weDisplay.value += event.target.innerText
@@ -14,12 +14,19 @@ function appendDigit(event){
         getResult()
     }else if(event.target.innerText === 'AC'){
         clearDisplay()
+    }else if(event.target.innerText === '+/-'){
+        getResult()
+        weDisplay.value = weDisplay.value * -1
     }
 }
 
 function getResult(){
-    operation = weDisplay.value.replace('X', '*').replace('÷', '/')
-    weDisplay.value = eval(operation)
+    operation = weDisplay.value.replace('X', '*').replace('÷', '/').replace(',', '.').replace('%', '/100')
+    try {
+        weDisplay.value = eval(operation)
+    }catch(e){
+        clearDisplay()
+    }
 }
 
 function clearDisplay() {
