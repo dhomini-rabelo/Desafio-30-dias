038a6d7f32811f3b5fc55caf2a1dc5c0eb1fa25a
Dhomini Fael <dhominisalgado@gmail.com>
2021-09-13 22:57:11 -0300

    mudança nome variavel e result de all adicionado

--- DAY#10/all.js
+++ DAY#10/all.js
@@ -1,22 +1,22 @@
-let school_data = {
+let schoolData = {
     'João': [10, 8.5, 7],
     'Ana': [7.5, 5, 6],
     'Alberto': [9, 8.5, 3],
     'Vitória': [4, 8, 9],
 }
 
-for (student of Object.keys(school_data)){
-    school_data[student] = (school_data[student].reduce((accumulator, note) => accumulator + note)/3).toFixed(2)
+for (student of Object.keys(schoolData)){
+    schoolData[student] = (schoolData[student].reduce((accumulator, note) => accumulator + note)/3).toFixed(2)
 }
 
-let approved = Object.keys(school_data).filter((student) => school_data[student] >= 7)
+let approved = Object.keys(schoolData).filter((student) => schoolData[student] >= 7)
 
-let disapproved = Object.keys(school_data).filter((student) => school_data[student] < 7)
+let disapproved = Object.keys(schoolData).filter((student) => schoolData[student] < 7)
 
 
-let reportOfApproved = approved.map((student) => ` ${student} com média ${school_data[student]}`)
+let reportOfApproved = approved.map((student) => ` ${student} com média ${schoolData[student]}`)
 
-let reportOfDisapproved = disapproved.map((student) => ` ${student} com média ${school_data[student]}`)
+let reportOfDisapproved = disapproved.map((student) => ` ${student} com média ${schoolData[student]}`)
 
 console.log(`Aprovados:${reportOfApproved}`)
 console.log(`Reprovados:${reportOfDisapproved}`)
